d7e594a841525cba0618f938ea1c94b5bf941a47
Shahroon Farooqi <msrfarooqi@gmail.com>
2021-08-25 02:15:08 +0500

    set async storage

--- src/context/UseData.js
+++ src/context/UseData.js
@@ -1,5 +1,6 @@
 //Import Libraires
 import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //Create Context
 const DataContext = createContext();
@@ -8,10 +9,27 @@ const DataContext = createContext();
 import mockup from "../utils/data";
 
 export const DataProvider = ({ children }) => {
-  const [transcations, setTranscations] = useState(mockup.customers);
+  const [transcations, setTranscations] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("transcations").then((_transcations) => {
+      if (_transcations !== null) {
+        setTranscations(JSON.parse(_transcations));
+      }
+    });
+  }, []);
 
   const addEntry = (_data) => {
     setTranscations([_data, ...transcations]);
+    addToLocal([_data, ...transcations]);
+  };
+
+  const addToLocal = async (_transcations) => {
+    try {
+      await AsyncStorage.setItem("transcations", JSON.stringify(_transcations));
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
